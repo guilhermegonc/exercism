510c505a76b8540dcb81c28483b30b2bd1d39029
guilhermegonc <gonc.guilherme@gmail.com>
2018-09-20 22:36:40 -0300

    Upload complex numbers' exercise

--- python/complex-numbers/complex_numbers.py
+++ python/complex-numbers/complex_numbers.py
@@ -1,18 +1,31 @@
-class ComplexNumber(object):
+class ComplexNumber:
     def __init__(self, real, imaginary):
-        pass
+        self.real = real
+        self.imaginary = imaginary
+
+    def __repr__(self):
+        return f'{self.real} + {self.imaginary}i'
+
+    def __eq__(self, other):
+        return self.real == other.real and self.imaginary == other.imaginary
 
     def __add__(self, other):
-        pass
+        return ComplexNumber(self.real + other.real, self.imaginary + other.imaginary)
 
     def __mul__(self, other):
-        pass
+        return ComplexNumber(
+            self.real * other.real + self.imaginary * other.imaginary * -1,
+            self.real * other.imaginary + self.imaginary * other.real
+        )
 
     def __sub__(self, other):
-        pass
+        return ComplexNumber(self.real - other.real, self.imaginary - other.imaginary)
 
     def __truediv__(self, other):
-        pass
+        return ComplexNumber(
+            (self.real * other.real + self.imaginary * other.imaginary) / (other.real ** 2 + other.imaginary ** 2),
+            -1 * self.real * other.imaginary + self.imaginary * other.real
+        )
 
     def __abs__(self):
         pass
@@ -22,3 +35,5 @@ class ComplexNumber(object):
 
     def exp(self):
         pass
+
+print(ComplexNumber(1.0, 0.0) / ComplexNumber(2.0, 0.0))
