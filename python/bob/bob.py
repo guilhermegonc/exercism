3ee79f5f8a5994af2e54160d2f8721c609409bd5
guilhermegonc <gonc.guilherme@gmail.com>
2018-09-22 10:00:52 -0300

    Exxercise first attempt

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,21 @@
+
 def hey(phrase):
-    pass
+    phrase = phrase.strip()
+    escape_special_chars(phrase)
+    print(phrase)
+    if phrase.isspace() or phrase == '':
+        return 'Fine. Be that way!'
+    elif phrase.isupper() and '?' in phrase[-1]:
+        return 'Calm down, I know what I\'m doing!'
+    elif '?' in phrase[-1]:
+        return 'Sure.'
+    elif phrase.isupper():
+        return 'Whoa, chill out!'
+    else:
+        return 'Whatever.'
+
+
+def escape_special_chars(text):
+    text = text.replace(r'\n', '')
+    text = text.replace(r'\t', '')
+    return text
