004d372c706e6b33e8d73f5245819574c88718d1
guilhermegonc <gonc.guilherme@gmail.com>
2018-10-06 16:41:15 -0300

    Improved code modularity by treating possibilities in restrict known scores method

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,19 +1,18 @@
-import pandas as pd
-
 class Allergies:
-
     def __init__(self, score):
+        self.score = score
         self.allergies_index = [
             ['eggs', 1], ['peanuts', 2], ['shellfish', 4], ['strawberries', 8],
             ['tomatoes', 16], ['chocolate', 32], ['pollen', 64], ['cats', 128]
         ]
-        self.score = score
-        self.restrict_known_scores()
-        self.populate_list_of_allergies()
 
     def restrict_known_scores(self):
-        if self.score > 255:
-            return self.score % 256
+        possibilities = 0
+        for allergies, scores in self.allergies_index:
+            possibilities += scores
+
+        if self.score > possibilities:
+            return self.score % (possibilities + 1)
         return self.score
 
     def populate_list_of_allergies(self):
@@ -23,7 +22,7 @@ class Allergies:
         while True:
             if top_score == 0:
                 break
-            for data in self.allergies_index[::-1]:  # Loop the scores to find the max viable
+            for data in self.allergies_index[::-1]:
                 if data[1] <= top_score:
                     allergies.append(data[0])
                     top_score -= data[1]
