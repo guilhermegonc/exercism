df160cd107850b28f9f6791b4ee3833ad440ed83
guilhermegonc <gonc.guilherme@gmail.com>
2018-10-06 14:45:59 -0300

    Exercises passing all tests

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,12 +1,35 @@
 class Allergies(object):
 
     def __init__(self, score):
-        pass
+        allergies_dict = {
+            'eggs': 1, 'peanuts': 2, 'shellfish': 4, 'strawberries': 8,
+            'tomatoes': 16, 'chocolate': 32, 'pollen': 64, 'cats': 128
+        }
+
+        self.score = score
+        self.valid_allergies_score = score
+
+        if self.score > 255:
+            self.valid_allergies_score = self.score % 256
+
+        self.allergies = []
+
+        top_score = allergy_score = self.valid_allergies_score
+        while True:
+            if top_score == 0:
+                break
+
+            for name, score_value in allergies_dict.items():
+                if score_value <= top_score:
+                    allergy_score = score_value
+                    valid_allergy = name
+
+            self.allergies.append(valid_allergy)
+            top_score -= allergy_score
 
     def is_allergic_to(self, item):
-        pass
+        return item in self.allergies
 
     @property
     def lst(self):
-        pass
-
+        return self.allergies
