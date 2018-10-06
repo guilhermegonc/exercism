a0f61b8664cc3b8d3515f5c3a3ce6a4e0c71a39e
guilhermegonc <gonc.guilherme@gmail.com>
2018-10-06 19:38:54 -0300

    Improve readability for rational exercises

--- python/rational-numbers/rational_numbers.py
+++ python/rational-numbers/rational_numbers.py
@@ -2,23 +2,16 @@ from __future__ import division
 
 
 class Rational:
-    def __init__(self, numer, denom):
-        gcd = 1
-        if numer == 0:
-            self.numer = numer
-            self.denom = 1
-        else:
-            for c in range(1, abs(numer) + 1):
-                if numer % c == 0 and denom % c == 0:
-                    gcd = c
-            numer = numer // gcd
-            denom = denom // gcd
-            if (numer > 0 and denom < 0) or (numer < 0 and denom < 0):
-                numer = -numer
-            if denom < 0:
-                denom = -denom
-            self.numer = numer
-            self.denom = denom
+    def __init__(self, numerator, denominator):
+        if numerator == 0:
+            numerator, denominator = 0, 1
+
+        if denominator < 0:
+            numerator, denominator = -numerator, -denominator
+
+        for c in range(1, denominator + 1):
+            if numerator % c == 0 and denominator % c == 0:
+                self.numer, self.denom = numerator // c, denominator // c
 
     def __eq__(self, other):
         return self.numer == other.numer and self.denom == other.denom
@@ -39,21 +32,16 @@ class Rational:
         return Rational(self.numer * other.denom, self.denom * other.numer)
 
     def __abs__(self):
-        numer = self.numer
-        denom = self.denom
-        if numer < 0:
-            numer = -self.numer
-        if denom < 0:
-            denom = -self.denom
-        return Rational(numer, denom)
+        if self.numer < 0:
+            return Rational(-self.numer, self.denom)
+        return Rational(self.numer, self.denom)
 
     def __pow__(self, power):
-        if power == 0:
-            return Rational(1, 1)
-        elif power > 0:
+        if power > 0:
             return Rational(self.numer ** power, self.denom ** power)
-        elif power < 0:
+        if power < 0:
             return Rational(self.denom ** power, self.numer ** power)
+        return Rational(1, 1)
 
     def __rpow__(self, base):
         return base ** (self.numer / self.denom)
