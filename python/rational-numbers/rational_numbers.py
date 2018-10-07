5e7ced901318ccd5e655c4189642a102327a1dc0
guilhermegonc <gonc.guilherme@gmail.com>
2018-10-07 12:56:05 -0300

    Improve readability for rational exercises

--- python/rational-numbers/rational_numbers.py
+++ python/rational-numbers/rational_numbers.py
@@ -11,37 +11,37 @@ class Rational:
 
         for c in range(1, denominator + 1):
             if numerator % c == 0 and denominator % c == 0:
-                self.numer, self.denom = numerator // c, denominator // c
+                self.numerator, self.denominator = numerator // c, denominator // c
 
     def __eq__(self, other):
-        return self.numer == other.numer and self.denom == other.denom
+        return self.numerator == other.numerator and self.denominator == other.denominator
 
     def __repr__(self):
         return f'{self.numer}/{self.denom}'
 
     def __add__(self, other):
-        return Rational(self.numer * other.denom + other.numer * self.denom, self.denom * other.denom)
+        return Rational(self.numerator * other.denominator + other.numerator * self.denominator, self.denominator * other.denominator)
 
     def __sub__(self, other):
-        return Rational(self.numer * other.denom - other.numer * self.denom, self.denom * other.denom)
+        return Rational(self.numerator * other.denominator - other.numerator * self.denominator, self.denominator * other.denominator)
 
     def __mul__(self, other):
-        return Rational(self.numer * other.numer, self.denom * other.denom)
+        return Rational(self.numerator * other.numerator, self.denominator * other.denominator)
 
     def __truediv__(self, other):
-        return Rational(self.numer * other.denom, self.denom * other.numer)
+        return Rational(self.numerator * other.denominator, self.denominator * other.numerator)
 
     def __abs__(self):
-        if self.numer < 0:
-            return Rational(-self.numer, self.denom)
-        return Rational(self.numer, self.denom)
+        if self.numerator < 0:
+            return Rational(-self.numerator, self.denominator)
+        return Rational(self.numerator, self.denominator)
 
     def __pow__(self, power):
         if power > 0:
-            return Rational(self.numer ** power, self.denom ** power)
+            return Rational(self.numerator ** power, self.denominator ** power)
         if power < 0:
-            return Rational(self.denom ** power, self.numer ** power)
+            return Rational(self.denominator ** power, self.numerator ** power)
         return Rational(1, 1)
 
     def __rpow__(self, base):
-        return base ** (self.numer / self.denom)
+        return base ** (self.numerator / self.denominator)
