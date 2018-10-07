225647855a684371224c0c7a892965d52e45063e
guilhermegonc <gonc.guilherme@gmail.com>
2018-10-07 20:30:20 -0300

    Improving readability for Hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,9 +1,10 @@
 def distance(strand_a, strand_b):
     counter = 0
-    if len(strand_a) == len(strand_b):
-        for c in range(len(strand_a)):
-            if strand_a[c] != strand_b[c]:
-                counter += 1
-    else:
-        raise ValueError('It is not possible to compare different string\'s lengths.')
+
+    if len(strand_a) != len(strand_b):
+        raise ValueError('It is not possible to compare different string lengths.')
+
+    for c in range(len(strand_a)):
+        if strand_a[c] != strand_b[c]:
+            counter += 1
     return counter
