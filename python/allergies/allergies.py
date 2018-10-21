63298db8e81dfcdd73045e8f4aba4d8d8d66c079
guilhermegonc <gonc.guilherme@gmail.com>
2018-10-20 21:49:25 -0300

    Refactor for Allergies

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -6,31 +6,27 @@ class Allergies:
             ['tomatoes', 16], ['chocolate', 32], ['pollen', 64], ['cats', 128]
         ]
 
-    def restrict_known_scores(self):
-        possibilities = 0
-        for allergies, scores in self.allergies_index:
-            possibilities += scores
-
-        if self.score > possibilities:
-            return self.score % (possibilities + 1)
+    def ignore_unknown_scores(self):  # Ignore part of the score that reaches or overpass 2 times the higher number.
+        if self.score >= (self.allergies_index[-1][1] * 2):
+            return self.score % (self.allergies_index[-1][1] * 2)
         return self.score
 
     def populate_list(self):
         allergies = []
-        top_score = self.restrict_known_scores()
+        top_score = self.ignore_unknown_scores()
 
-        while True:
-            if top_score == 0:
-                break
-            for data in self.allergies_index[::-1]:
-                if data[1] <= top_score:
-                    allergies.append(data[0])
-                    top_score -= data[1]
+        if top_score == 0:
+            return allergies
+
+        for data in self.allergies_index[::-1]:  # Decreases the higher viable number from score and appends its value.
+            if data[1] <= top_score:
+                allergies.append(data[0])
+                top_score -= data[1]
         return allergies
 
     def is_allergic_to(self, item):
-        return item in self.populate_list_of_allergies()
+        return item in self.populate_list()
 
     @property
     def lst(self):
-        return self.populate_list_of_allergies()
+        return self.populate_list()
