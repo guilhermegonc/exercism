1f19fdd115ef729d7491353dfd1dde13be9fa0a8
guilhermegonc <gonc.guilherme@gmail.com>
2018-10-21 00:24:45 -0300

    Rotational Cipher solved exercise

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,16 @@
+from string import ascii_letters, ascii_lowercase
+
+
 def rotate(text, key):
-    pass
+    ascii_rotate = ['_' for _ in range(26)]
+
+    for pos, ch in enumerate(ascii_lowercase):
+        if pos - key <= 25:
+            ascii_rotate[pos - key] = ch
+        else:
+            ascii_rotate[(pos - key) % 26] = ch
+
+    ascii_rotate = ''.join(ascii_rotate) + ''.join(ascii_rotate).upper()
+    transcription = str.maketrans(ascii_letters, ascii_rotate)
+
+    return str.translate(text, transcription)
