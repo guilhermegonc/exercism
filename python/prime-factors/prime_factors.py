1a364f57815ab0cdb939e683642927fba4fc349f
guilhermegonc <gonc.guilherme@gmail.com>
2018-10-21 21:20:43 -0300

    Anagram resolution

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,12 +1,14 @@
 def prime_factors(natural_number):
     divisor = 2
-    prime_list = list()
-    while True:
+    prime_list = []
+
+    while natural_number > 1:
+
         if natural_number % divisor == 0:
             natural_number /= divisor
             prime_list.append(divisor)
-        elif natural_number / divisor > 1:
-            divisor += 1
+
         else:
-            break
+            divisor += 1
+
     return prime_list
