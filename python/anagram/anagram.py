bfef2004f46c21537ca7418e35a2f438c0a6cfec
guilhermegonc <gonc.guilherme@gmail.com>
2018-10-28 20:26:55 -0300

    Legibility improvements for anagram

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,8 +1,6 @@
 def find_anagrams(word, candidates):
     true_anagrams = []
-
     for to_test in candidates:
-        if word.lower() != to_test.lower() and sorted(list(word.lower())) == sorted(list(to_test.lower())):
-            true_anagrams.append(to_test)
-
+        if sorted(word.lower()) == sorted(to_test.lower()) and word.lower() != to_test.lower():
+                true_anagrams.append(to_test)
     return true_anagrams
