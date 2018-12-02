b3a456309e133f323f844e7a0f7702f3ad7afcf4
guilhermegonc <gonc.guilherme@gmail.com>
2018-12-02 12:58:13 -0200

    Twelve Days Exercise

--- python/beer-song/beer_song.py
+++ python/beer-song/beer_song.py
@@ -4,24 +4,21 @@ def recite(start, take=1):
 
     for i in range(take):
 
-        if num_bottles > 1:
-            part_1 = f'{num_bottles} bottles of beer on the wall, {num_bottles} bottles of beer.'
-        elif num_bottles == 1:
-            part_1 = f'{num_bottles} bottle of beer on the wall, {num_bottles} bottle of beer.'
-        else:
-            part_1 = f'No more bottles of beer on the wall, no more bottles of beer.'
-        lst.append(part_1)
+        part_1 = f'{num_bottles if num_bottles > 0 else "No more"} ' \
+                 f'{"bottle" if num_bottles == 1 else "bottles"} of beer on the wall, ' \
+                 f'{num_bottles if num_bottles > 0 else "no more"} ' \
+                 f'{"bottle" if num_bottles == 1 else "bottles"} of beer.'
         num_bottles -= 1
 
-        if num_bottles > 1:
-            part_2 = f'Take one down and pass it around, {num_bottles} bottles of beer on the wall.'
-        elif num_bottles == 1:
-            part_2 = f'Take one down and pass it around, {num_bottles} bottle of beer on the wall.'
-        elif num_bottles == 0:
-            part_2 = 'Take it down and pass it around, no more bottles of beer on the wall.'
+        if num_bottles >= 0:
+            part_2 = f'Take {"it" if num_bottles == 0 else "one"} down and pass it around, ' \
+                     f'{num_bottles if num_bottles != 0 else "no more"} ' \
+                     f'{"bottle" if num_bottles == 1 else "bottles"} of beer on the wall.'
         else:
             part_2 = 'Go to the store and buy some more, 99 bottles of beer on the wall.'
             num_bottles = 99
+
+        lst.append(part_1)
         lst.append(part_2)
 
         if i < take - 1:
