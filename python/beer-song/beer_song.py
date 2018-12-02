8bb1d2376d00f866ee6655b9bc0c11e8101727f4
guilhermegonc <gonc.guilherme@gmail.com>
2018-12-01 22:09:47 -0200

    Beer Song Exercise

--- python/beer-song/beer_song.py
+++ python/beer-song/beer_song.py
@@ -1,23 +1,23 @@
 def recite(start, take=1):
-    bottles = start
+    num_bottles = start
     lst = []
 
     for i in range(take):
 
-        if bottles > 1:
-            part_1 = f'{bottles} bottles of beer on the wall, {bottles} bottles of beer.'
-        elif bottles == 1:
-            part_1 = f'{bottles} bottle of beer on the wall, {bottles} bottle of beer.'
+        if num_bottles > 1:
+            part_1 = f'{num_bottles} bottles of beer on the wall, {num_bottles} bottles of beer.'
+        elif num_bottles == 1:
+            part_1 = f'{num_bottles} bottle of beer on the wall, {num_bottles} bottle of beer.'
         else:
             part_1 = f'No more bottles of beer on the wall, no more bottles of beer.'
         lst.append(part_1)
-        bottles -= 1
+        num_bottles -= 1
 
-        if bottles > 1:
-            part_2 = f'Take one down and pass it around, {bottles} bottles of beer on the wall.'
-        elif bottles == 1:
-            part_2 = f'Take one down and pass it around, {bottles} bottle of beer on the wall.'
-        elif bottles == 0:
+        if num_bottles > 1:
+            part_2 = f'Take one down and pass it around, {num_bottles} bottles of beer on the wall.'
+        elif num_bottles == 1:
+            part_2 = f'Take one down and pass it around, {num_bottles} bottle of beer on the wall.'
+        elif num_bottles == 0:
             part_2 = 'Take it down and pass it around, no more bottles of beer on the wall.'
         else:
             part_2 = 'Go to the store and buy some more, 99 bottles of beer on the wall.'
