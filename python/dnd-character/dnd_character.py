a6d6ee4b62b2531cf8655a0f37f4b534f7b87684
guilhermegonc <gonc.guilherme@gmail.com>
2019-01-02 21:01:22 -0200

    except improvements

---
+++ python/dnd-character/dnd_character.py
@@ -0,0 +1,29 @@
+from random import randint
+from math import floor
+
+
+class Character:
+    def __init__(self):
+        ability_values = [self.ability() for _ in range(6)]
+        self.strength, self.dexterity, self.constitution, \
+        self.intelligence, self.wisdom, self.charisma = ability_values
+
+        self.hitpoints = 10 + modifier(self.constitution)
+
+    def __repr__(self):
+        return f'Strength: {self.strength}\n' \
+               f'Dexterity: {self.dexterity}\n' \
+               f'Constitution: {self.constitution}\n' \
+               f'Intelligence: {self.intelligence}\n' \
+               f'Wisdom: {self.wisdom}\n' \
+               f'Charisma: {self.charisma}\n\n' \
+               f'Hitpoints: {self.hitpoints}'
+
+    def ability(self):
+        values = [randint(1, 6) for i in range(4)]
+        values = sorted(values)
+        return sum(values[1:])
+
+
+def modifier(num):
+    return floor((num - 10) / 2)
