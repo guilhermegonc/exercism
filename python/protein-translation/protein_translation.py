f0a696f0ea099b72360e7b56e80a9568a24c316f
gonc.guilherme@gmail.com <P@pagaio1>
2019-04-27 17:30:23 -0300

    protein exercise

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -5,14 +5,14 @@ def proteins(strand):
         'UUA': 'Leucine', 'UUG': 'Leucine', 'UCU': 'Serine',
         'UCC': 'Serine', 'UCA': 'Serine', 'UCG': 'Serine',
         'UAU': 'Tyrosine', 'UAC': 'Tyrosine', 'UGU': 'Cysteine',
-        'UGC': 'Cysteine', 'UGG': 'Tryptophan'
+        'UGC': 'Cysteine', 'UGG': 'Tryptophan', 'UAA': 'STOP',
+        'UAG': 'STOP', 'UGA': 'STOP'
     }
 
-    list_of_codon = [strand[i: i+3] for i in range(0, len(strand), 3)]
-    for cod in list_of_codon:
+    list_of_codons = [strand[i: i+3] for i in range(0, len(strand), 3)]
+    list_of_proteins = [prot_dict[cod] for cod in list_of_codons]
 
-        if cod in ('UAA', 'UAG', 'UGA'):
-            return list_of_proteins
-
-        list_of_proteins.append(prot_dict[cod])
+    if 'STOP' in list_of_proteins:
+        stop_position = list_of_proteins.index('STOP')
+        return list_of_proteins[:stop_position]
     return list_of_proteins
