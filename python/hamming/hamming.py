f369aa33dc0c0e95a752948e2c4dc2d06f27f273
gonc.guilherme@gmail.com <P@pagaio1>
2019-04-26 21:57:58 -0300

    except improvements

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,10 +1,9 @@
 def distance(strand_a, strand_b):
-    counter = 0
-
     if len(strand_a) != len(strand_b):
         raise ValueError('It is not possible to compare different string lengths.')
 
-    for c in range(len(strand_a)):
-        if strand_a[c] != strand_b[c]:
+    counter = 0
+    for a, b in zip(strand_a, strand_b):
+        if a != b:
             counter += 1
     return counter
