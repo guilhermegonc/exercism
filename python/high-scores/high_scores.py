48e64a4aa94625d0942d4d55013459127616e84d
gonc.guilherme@gmail.com <P@pagaio1>
2019-04-28 17:24:57 -0300

    refactor for high scores using sorted function

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -9,12 +9,5 @@ class HighScores(object):
         return max(self.scores)
 
     def personal_top_three(self):
-        top_three = []
-        values_to_check = self.scores.copy()
-
-        loop_size = 3 if len(values_to_check) >= 3 else len(values_to_check)
-        for i in range(loop_size):
-            top_value = max(values_to_check)
-            top_three.append(top_value)
-            values_to_check.remove(top_value)
-        return top_three
+        top_three_len = 3 if len(self.scores) >= 3 else len(self.scores)
+        return sorted(self.scores, reverse=True)[:top_three_len]
