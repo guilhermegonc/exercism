4384669e9d6a1b233b00a6e4694ff625187ac7e4
guilherme <gonc.guilherme@mesalva.com>
2019-05-20 23:08:42 -0300

    New exercise added: Series.

---
+++ python/series/series.py
@@ -0,0 +1,13 @@
+def slices(series, length):
+    list_of_subs = []
+    begin, end = 0, length
+
+    if length > len(series) or length <= 0:
+        raise ValueError("Substring's length must be bigger than zero and smaller than series' length")
+
+    while end <= len(series) and length > 0:
+        list_of_subs.append(series[begin:end])
+        begin += 1
+        end += 1
+
+    return list_of_subs
