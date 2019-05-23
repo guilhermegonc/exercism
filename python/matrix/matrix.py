9da6ed1b2020544b9a9858d98fce8b20acd3c5b0
guilherme <gonc.guilherme@mesalva.com>
2019-05-23 20:59:54 -0300

    Matrix

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,14 +1,14 @@
 class Matrix(object):
     def __init__(self, matrix_string):
         self.matrix_string = matrix_string
-        self.number_of_rows = len(self.matrix_string.split('\n'))
+        self.row_split = self.matrix_string.split('\n')
 
     def __repr__(self):
         return str(self.matrix_string)
 
     def row(self, index):
-        string_list = self.matrix_string.split('\n')[index - 1].split()
-        return [int(i) for i in string_list]
+        selected_row = self.row_split[index - 1]
+        return [int(i) for i in selected_row.split()]
 
     def column(self, index):
-        return [self.row(i + 1)[index - 1] for i in range(self.number_of_rows)]
+        return [self.row(i)[index - 1] for i in range(1, len(self.row_split) + 1)]
