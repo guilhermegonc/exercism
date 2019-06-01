a591275c6ee2986ba0aaf1930afef3cecab46385
guilherme <gonc.guilherme@mesalva.com>
2019-06-01 10:54:27 -0300

    tree building constructor

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -2,10 +2,14 @@ def raindrops(number):
     string = ''
     if number % 3 == 0:
         string += 'Pling'
+
     if number % 5 == 0:
         string += 'Plang'
+
     if number % 7 == 0:
         string += 'Plong'
+
     if len(string) == 0:
         return str(number)
+
     return string
