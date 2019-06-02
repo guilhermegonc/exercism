a7152fcbd9838972575a942be3565df9e73a7a40
guilherme <gonc.guilherme@mesalva.com>
2019-06-02 20:10:26 -0300

    saddle points submission

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,31 @@
 def saddle_points(matrix):
-    pass
+    if len(matrix) == 0:
+        return [{}]
+    if len(matrix) > 0:
+        test = len(matrix[0])
+        for r in matrix:
+            if len(r) != test:
+                raise ValueError('Irregular matrix is not valid.')
+
+    columns = []
+    for i in range(len(matrix[0])):
+        columns.append([row[i] for row in matrix])
+
+    candidates = {}
+    for row_index, r in enumerate(matrix):
+        for column_index, c in enumerate(r):
+            if c == max(r):
+                candidates[(row_index, column_index)] = c
+
+    answer = []
+    for c in candidates.items():
+        if c[1] <= min(columns[c[0][1]]):
+            bola = {'row': c[0][0] + 1, 'column': c[0][1] + 1}
+            answer.append(bola.copy())
+    if len(answer) == 0:
+        return [{}]
+    return answer
+
+
+matrix = [[]]
+print(saddle_points(matrix))
