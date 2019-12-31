049c72e15c2ecf5117040e7bc484f293ab9ad7ad
guilherme <gonc.guilherme@mesalva.com>
2019-12-31 20:46:52 -0300

    acronym submission

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,27 +1,29 @@
 class Garden:
     def __init__(self, diagram, students=None):
-        self.diagram = diagram
-        self.plants_dct = {'G': 'Grass', 'C': 'Clover', 'R': 'Radishes', 'V': 'Violets'}
-        self.students = students if students else ['Alice', 'Bob', 'Charlie', 'David',
-                                                   'Eve', 'Fred', 'Ginny', 'Harriet',
-                                                   'Ileana', 'Joseph', 'Kincaid', 'Larry']
+        self.plants_dct = {
+            'G': 'Grass',
+            'C': 'Clover',
+            'R': 'Radishes',
+            'V': 'Violets'
+        }
+        self.diagram = diagram.replace('\n', '')
+        self.row_size = len(self.diagram) / 2
+        self.students = sorted(students) if students else ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred',
+                                                           'Ginny', 'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry']
 
     def plants(self, child):
-        first_pos, last_pos = self.set_limits(child)
-        rows = self.split_rows()
+        first_row, second_row = self.set_positions(child)
+        plants_list = [self.diagram[first_row], self.diagram[first_row + 1],
+                       self.diagram[second_row], self.diagram[second_row + 1]]
+        return [self.plants_dct[p] for p in plants_list]
 
-        first_row = [self.plants_dct[p] for p in rows[0][first_pos:last_pos]]
-        second_row = [self.plants_dct[p] for p in rows[1][first_pos:last_pos]]
+    def set_positions(self, child):
+        reference = self.find_child_position(child)
 
-        return first_row + second_row
+        first_row = int(reference * 2)
+        second_row = int(first_row + self.row_size)
 
-    def set_limits(self, child):
-        position = self.find_child_position(child)
-        return position * 2, position * 2 + 2
+        return first_row, second_row
 
     def find_child_position(self, child):
-        self.students.sort()
         return self.students.index(child)
-
-    def split_rows(self):
-        return self.diagram.split('\n')
